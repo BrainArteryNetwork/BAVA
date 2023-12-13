7b52acf8d0381d194af8c4c7ef4bff71a68cf28f
Sharan Ranjit S <sharanranjit@gmail.com>
2023-12-13 01:46:15 -0800

    Added setup.py to help setting and installation of repo

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+"""
+from setuptools import setup, find_packages
+PACKAGES = find_packages()
+
+opts = dict(
+    name='BAVA',
+    version='0.1',
+    url='https://github.com/BrainArteryNetwork/BAVA',
+    license='MIT',
+    author='BrainArteryNetwork',
+    author_email='kennyzhang315cn@gmail.com',
+    description='Brain Artery Visualization & Analysis Tool',
+    packages=PACKAGES,
+    package_data={'bava': ['api/*', 'streamlit/*', 'visualization3d/*', 'tests/*']}
+
+)
+
+if __name__ == '__main__':
+    setup(**opts)
